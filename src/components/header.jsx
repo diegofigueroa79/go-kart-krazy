b9a94d020746e7ee49621506bcfbed97d832e143
Diego Figueroa <diegofigueroa79@gmail.com>
2023-06-20 20:56:24 -0700

    add header

---
+++ src/components/header.jsx
@@ -0,0 +1,33 @@
+export default function Header() {
+  return (
+    <>
+      <header>
+        <span>
+          <h1>Go Kart Krazy!</h1>
+        </span>
+        <span>
+          <a href="https://diegofigueroa79.github.io/portfolio-frontend/" rel="noreferrer" target="_blank">
+            <i class="devicon-github-original-wordmark colored"></i>
+          </a>
+        </span>
+        <span>
+          <a href="https://www.linkedin.com/in/diegofigueroa79/" rel="noreferrer" target="_blank">
+            <i class="devicon-linkedin-plain-wordmark"></i>
+          </a>
+        </span>
+      </header>
+      <div className="sub-header">
+        <span>
+          <a href="https://diegofigueroa79.github.io/portfolio-frontend/" rel="noreferrer" target="_blank">
+            <i class="devicon-github-original-wordmark colored"></i>
+          </a>
+        </span>
+        <span>
+          <a href="https://www.linkedin.com/in/diegofigueroa79/" rel="noreferrer" target="_blank">
+            <i class="devicon-linkedin-plain-wordmark"></i>
+          </a>
+        </span>
+      </div>
+    </>
+  )
+}
