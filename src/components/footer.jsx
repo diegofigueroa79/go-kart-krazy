8530dc7893fe9ea05cd432125a7a3198df80b22d
Diego Figueroa <diegofigueroa79@gmail.com>
2023-06-20 20:56:10 -0700

    add footer

---
+++ src/components/footer.jsx
@@ -0,0 +1,13 @@
+export default function Footer() {
+  return (
+    <footer>
+      <span>Diego Figueroa 2023</span>
+      <span>|</span>
+      <span>
+        <a href="https://www.linkedin.com/in/diegofigueroa79/" rel="noreferrer" target="_blank">
+          <i class="devicon-linkedin-plain-wordmark"></i>
+        </a>
+      </span>
+    </footer>
+  )
+}
