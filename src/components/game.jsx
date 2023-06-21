fe30d1e340ee5feb449cb9f0f129892ef1fd98c7
Diego Figueroa <diegofigueroa79@gmail.com>
2023-06-20 22:57:58 -0700

    fix broken config links

--- src/components/game.jsx
+++ src/components/game.jsx
@@ -3,11 +3,13 @@ import { Unity, useUnityContext } from "react-unity-webgl"
 
 export default function Game() {
     const { unityProvider, isLoaded, loadingProgression } = useUnityContext({
-        loaderUrl: "go-kart-krazy/build/go-kart-crazy-build.loader.js",
-        dataUrl: "go-kart-krazy/build/go-kart-crazy-build.data.unityweb",
-        frameworkUrl: "go-kart-krazy/build/go-kart-crazy-build.framework.js.unityweb",
-        codeUrl: "go-kart-krazy/build/go-kart-crazy-build.wasm.unityweb"
+        loaderUrl: "https://diegofigueroa79.github.io/go-kart-krazy/build/go-kart-crazy-build.loader.js",
+        dataUrl: "https://diegofigueroa79.github.io/go-kart-krazy/build/go-kart-crazy-build.data.unityweb",
+        frameworkUrl: "https://diegofigueroa79.github.io/go-kart-krazy/build/go-kart-crazy-build.framework.js.unityweb",
+        codeUrl: "https://diegofigueroa79.github.io/go-kart-krazy/build/go-kart-crazy-build.wasm.unityweb"
     })
+
+    console.log(unityProvider)
 
     const loadingPercentage = Math.round(loadingProgression * 100);
     
